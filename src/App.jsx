dcd3b7fade6a11ec94091a7877b9afaac71f7cd8
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-19 23:47:21 +0300

    Added routes for future components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { Route, Routes } from "react-router-dom"
+import Layout from "./components/Layout/Layout"
+import Auth from "./components/Auth/Auth"
+import Profile from './components/Profile/Profile';
+import Catalog from './components/Catalog/Catalog';
+import RequestPage from './components/RequestPage/RequestPage';
 
 function App() {
 
   return (
-    <>
-    </>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/request-page" element={<RequestPage />} />
+        </Routes>
+      </Layout>
   )
 }
 
