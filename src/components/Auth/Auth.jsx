8818f7c3a5ed37e66f5bfa7a9b7a43e0efa05a10
Olya Khromova <olya.hrmvlg@gmail.com>
2025-01-22 22:52:37 +0300

    Refactor authentication form styles and update input placeholders

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -6,12 +6,12 @@ export default function Auth() {
                 <form className="auth__form form">
                     <h2 className="form__title">Вход</h2>
                     <div className="form__input-wrapper">
-                        <label className="form__label" htmlFor="login" placeholder="Введите e-mail" aria-placeholder="Введите e-mail">Логин</label>
-                        <input type="email" className="form__input" name="login" />
+                        <label className="form__label" htmlFor="login"  >Логин</label>
+                        <input type="email" className="form__input" placeholder="Введите e-mail" name="login" />
                     </div>
                     <div className="form__input-wrapper">
-                        <label className="form__label" htmlFor="password" placeholder="Введите пароль" aria-placeholder="Введите пароль">Пароль</label>
-                        <input type="password" className="form__input" name="password" />
+                        <label className="form__label" htmlFor="password" >Пароль</label>
+                        <input type="password" className="form__input" placeholder="Введите пароль" name="password" />
                     </div>
                     <button type="submit" className="form__button button button-accent">Войти</button>
                 </form>
